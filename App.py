9a3b9c3000f1a2a330d6e120639651589c9416a0
deepu2431 <deepakkumar27559@gmail.com>
2026-04-15 15:05:24 +0530

    Add image display to App.py

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from transformers import pipeline
-
+st.image(""C:\Users\DEEPU\OneDrive\Pictures\images.jpeg"")
 @st.cache_resource
 def load_summarizer():
   return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
